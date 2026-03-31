cecd901ecb7a69635c21eb774ffd2fd14c196850
Renan Moreira <renanmoreira.tech@gmail.com>
2026-03-31 00:05:23 -0300

    Add IAM roles and policies for EC2 and SSM

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,1 +1,60 @@
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "${var.project_name}-ec2-ssm-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}-ec2-ssm-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_ssm_core" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_ssm_instance_profile" {
+  name = "${var.project_name}-ec2-ssm-instance-profile"
+  role = aws_iam_role.ec2_ssm_role.name
+
+  tags = {
+    Name = "${var.project_name}-ec2-ssm-instance-profile"
+  }
+}
+
+resource "aws_iam_role" "ssm_maintenance_window_role" {
+  name = "${var.project_name}-ssm-maintenance-window-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ssm.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}-ssm-maintenance-window-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_maintenance_window_service_role" {
+  role       = aws_iam_role.ssm_maintenance_window_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMMaintenanceWindowRole"
+}
