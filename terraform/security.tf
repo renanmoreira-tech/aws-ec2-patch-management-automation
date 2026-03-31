c51653476b6cbfb7ed08fd8ff2022503e34b2e72
Renan Moreira <renanmoreira.tech@gmail.com>
2026-03-31 00:08:56 -0300

    Create security group for EC2 instances
    Add a security group for EC2 instances with SSH access and allow all outbound traffic.

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,1 +1,25 @@
+resource "aws_security_group" "ec2" {
+  name        = "${var.project_name}-ec2-sg"
+  description = "Security group for EC2 instances managed by Systems Manager"
+  vpc_id      = data.aws_subnet.az1.vpc_id
 
+  ingress {
+    description = "Optional SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ssh_cidr]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-ec2-sg"
+  }
+}
