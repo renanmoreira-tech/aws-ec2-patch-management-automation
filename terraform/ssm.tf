da851417e5fa89c7cf77be7e67ed2e490f639a73
Renan Moreira <renanmoreira.tech@gmail.com>
2026-03-31 00:17:18 -0300

    Create SSM patch baseline and group for Linux
    Added AWS SSM patch baseline and patch group resources for Linux instances.

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -1,1 +1,31 @@
+resource "aws_ssm_patch_baseline" "linux" {
+  name             = "${var.project_name}-${var.patch_group}-baseline"
+  description      = "Patch baseline for Linux instances in the patch management demo"
+  operating_system = "AMAZON_LINUX_2"
 
+  approval_rule {
+    approve_after_days = 7
+    compliance_level   = "CRITICAL"
+
+    patch_filter {
+      key    = "CLASSIFICATION"
+      values = ["Security", "Bugfix", "Critical"]
+    }
+
+    patch_filter {
+      key    = "SEVERITY"
+      values = ["Critical", "Important"]
+    }
+  }
+
+  approved_patches_enable_non_security = false
+
+  tags = {
+    Name = "${var.project_name}-${var.patch_group}-baseline"
+  }
+}
+
+resource "aws_ssm_patch_group" "linux_prod" {
+  baseline_id = aws_ssm_patch_baseline.linux.id
+  patch_group = var.patch_group
+}
